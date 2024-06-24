e63de3504d8a6200e0c5d2103ba5cba37485cd31
Elena Akimova <kulikovaelena19@yandex.ru>
2024-06-24 23:51:55 +0300

    add steps

--- src/test/java/pages/AutomationPracticeFormPage.java
+++ src/test/java/pages/AutomationPracticeFormPage.java
@@ -36,74 +36,88 @@ public class AutomationPracticeFormPage {
         return this;
     }
 
+    @Step("Вводим FirstName")
     public AutomationPracticeFormPage setFirstName(String value) {
         firstNameInput.setValue(value);
         return this;
     }
 
+    @Step("Вводим LastName")
     public AutomationPracticeFormPage setLastName(String value) {
         lastNameInput.setValue(value);
         return this;
     }
 
+    @Step("Вводим Email")
     public AutomationPracticeFormPage setEmail(String value) {
         userEmailInput.setValue(value);
         return this;
     }
 
+    @Step("Выбираем Gender")
     public AutomationPracticeFormPage setGender(String value) {
         genderWrapper.$(byText(value)).click();
         return this;
     }
 
+    @Step("Вводим MobileNumber")
     public AutomationPracticeFormPage setNumber(String value) {
         userNumberInput.setValue(value);
         return this;
     }
 
+    @Step("Выбираем DateOfBirth")
     public AutomationPracticeFormPage setDateOfBirth(String day, String month, String year) {
         calendarInput.click();
         calendarComponent.setDate(day, month, year);
         return this;
     }
 
+    @Step("Добавляем Subjects")
     public AutomationPracticeFormPage setSubjects(String value) {
         subjectsInput.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Выбираем Hobbies")
     public AutomationPracticeFormPage setHobbies(String value) {
         hobbiesWrapper.$(byText(value)).click();
         return this;
     }
 
+    @Step("Вводим CurrentAddress")
     public AutomationPracticeFormPage setCurrentAddress(String value) {
         currentAddressInput.setValue(value);
         return this;
     }
 
+    @Step("Выбираем State")
     public AutomationPracticeFormPage setState(String value) {
         selectState.click();
         $(byText(value)).click();
         return this;
     }
 
+    @Step("Выбираем City")
     public AutomationPracticeFormPage setCity(String value) {
         selectCity.click();
         $(byText(value)).click();
         return this;
     }
 
+    @Step("Загружаем Picture")
     public AutomationPracticeFormPage addPicture(String value) {
         pictureInput.uploadFromClasspath(value);
         return this;
     }
 
+    @Step("Нажимаем submit")
     public AutomationPracticeFormPage submit() {
         submitButton.click();
         return this;
     }
 
+    @Step("Проверяем форму Thanks for submitting the form")
     public AutomationPracticeFormPage checkResult(String key, String value) {
         tableResponsive.$(byText(key)).parent()
                 .shouldHave(text(value));
@@ -111,20 +125,24 @@ public class AutomationPracticeFormPage {
         return this;
     }
 
+
     public AutomationPracticeFormPage errorFistName() {
         firstNameInput.shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         return this;
     }
+
 
     public AutomationPracticeFormPage errorLastName() {
         lastNameInput.shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         return this;
     }
 
+
     public AutomationPracticeFormPage errorGender() {
         genderWrapper.$("label").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         return this;
     }
+
 
     public AutomationPracticeFormPage errorUserNumber() {
         userNumberInput.shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));

a49cefde09a5adf523262f4a25a89032a94d5ec7
Elena Akimova <kulikovaelena19@yandex.ru>
2024-06-27 10:16:32 +0300

    change browserSize

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -16,7 +16,7 @@ public class BaseTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "2560x1440";
         Configuration.baseUrl = "https://demoqa.com";
 
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
